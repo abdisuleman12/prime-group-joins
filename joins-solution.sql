ceffe64a397e797dd172ded27f982dd663e7c9d8
Abdi Suleman <abdisuleman12@gmail.com>
2017-08-16 16:53:20 -0500

    prime group hw

---
+++ joins-solution.sql
@@ -0,0 +1,20 @@
+--Get all customers and their addresses.--
+
+SELECT *
+FROM addresses
+JOIN customers ON addresses.customer_id = customers.id;
+
+--Get all orders and their line items.--
+
+--Which warehouses have cheetos?--
+
+
+--Which warehouses have diet pepsi?--
+
+--Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.--
+
+--How many customers do we have?--
+
+--How many products do we carry?--
+
+--What is the total available on-hand quantity of diet pepsi?--
